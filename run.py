0d6553370132e11bfd5b37cbedd0c2b1c28abcda
Joao Paulo Genari <jpgenari@gmail.com>
2023-07-23 14:44:16 +0100

    Calcuate surplus data

--- run.py
+++ run.py
@@ -76,7 +76,13 @@ def calculate_surplus_data(sales_row):
     print("Calculate surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    pprint(stock_row)
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 def main():
     """
@@ -85,7 +91,9 @@ def main():
     data = get_sales_data()
     sales_sata = [int(num) for num in data]
     update_sales_worksheet(sales_sata)
-    calculate_surplus_data(sales_sata)
+    new_surplus_data = calculate_surplus_data(sales_sata)
+    print(new_surplus_data)
+
 
 print("Welcome to love Sandwiches Data Automation")
 main()
